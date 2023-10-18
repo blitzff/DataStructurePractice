fa013ed940588f6a6a3d7b83a4903ed1b055c2c9
xufeng <zzhxufeng@163.com>
2023-10-18 22:40:42 +0800

    MaxAfterKOps

---
+++ DataStructurePractice/LeetCode/MaxAfterKOps.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStructurePractice.LeetCode;
+
+public class MaxAfterKOps
+{
+    public long MaxKelements(int[] nums, int k)
+    {
+        var maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b - a));
+        for (int i = 0; i < nums.Length; i++)
+        {
+            maxHeap.Enqueue(nums[i], nums[i]);
+        }
+
+        var score = 0l;
+        for (var i = 0; i < k; i++)
+        {
+            var max = maxHeap.Dequeue();
+            score += max;
+            var newNum = (int)Math.Ceiling((double)max / 3);
+            maxHeap.Enqueue(newNum, newNum);
+        }
+        return score;
+    }
+
+}
