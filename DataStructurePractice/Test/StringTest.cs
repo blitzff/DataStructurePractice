aef4dd10ffcb3b307c45bbc4488d952d7b0f5718
xufeng <zzhxufeng@163.com>
2024-02-01 13:24:54 +0800

    string ref

---
+++ DataStructurePractice/Test/StringTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePractice.Test
+{
+    [TestClass]
+    public class StringTest
+    {
+        [TestMethod]
+        public void test()
+        {
+            var str1 = "abc";
+            var str2 = str1;
+
+            str1 = "def";
+
+            Console.WriteLine(str2); // "def"
+        }
+    }
+}
